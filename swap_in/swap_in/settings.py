c507b60079ed481be85cca120d7b0f97b7872713
ag94e <a.guerrero.esp94@gmail.com>
2020-08-23 20:39:09 -0500

    add remote db

--- swap_in/swap_in/settings.py
+++ swap_in/swap_in/settings.py
@@ -95,23 +95,23 @@ WSGI_APPLICATION = 'swap_in.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'hackathon',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'swapintest',
+        'USER': 'admin',
+        'PASSWORD': 'swapin_test123',
+        'HOST': 'swapintest.cbe8bosjmboa.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
