bb33859787827690513f4cf7587c72a29922aba2
ag94e <a.guerrero.esp94@gmail.com>
2020-08-23 23:24:07 -0500

    modified static deploy

--- swap_in/swap_in/settings.py
+++ swap_in/swap_in/settings.py
@@ -151,6 +151,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_RENDERER_CLASSES': (
